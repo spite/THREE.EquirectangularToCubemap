2a2b71c7340dd897472ec530df563b608f7c923d
Jaume Sanchez <the.spite@gmail.com>
2016-10-07 19:59:07 +0200

    updated build

--- build/EquirectangularToCubemap.js
+++ build/EquirectangularToCubemap.js
@@ -1,5 +1,14 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-( function() {
+;(function() {
+
+	"use strict";
+
+	var root = this
+
+	var has_require = typeof require !== 'undefined'
+
+	var THREE = root.THREE || has_require && require('three')
+	if( !THREE )
+		throw new Error( 'EquirectangularToCubemap requires three.js' )
 
 function EquirectangularToCubemap( renderer ) {
 
@@ -36,8 +45,14 @@ EquirectangularToCubemap.prototype.convert = function( source, size ) {
 
 }
 
-window.THREE.EquirectangularToCubemap = EquirectangularToCubemap;
+if( typeof exports !== 'undefined' ) {
+	if( typeof module !== 'undefined' && module.exports ) {
+		exports = module.exports = EquirectangularToCubemap
+	}
+	exports.EquirectangularToCubemap = EquirectangularToCubemap
+}
+else {
+	root.EquirectangularToCubemap = EquirectangularToCubemap
+}
 
-} )();
-
-},{}]},{},[1]);
+}).call(this);
